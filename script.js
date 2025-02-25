094cb4b1217e18584e4c0dd2d72d0e88fdad8cea
srinuvasKonchada <srisri201318@gmail.com>
2025-02-25 15:51:05 +0530

    task

---
+++ script.js
@@ -0,0 +1,171 @@
+//variables 
+var color="white";
+let name="srinivas";
+const number=2013;
+
+console.log(color);
+console.log(name);
+console.log(number);
+  
+//2.DataTypes
+// String
+let num = "42";  
+console.log(num);
+console.log(typeof num);
+
+//Number
+let age = 42;
+console.log(age);
+console.log(typeof age);
+
+//Boolean
+let isAvailable = true;  
+console.log(isAvailable);
+console.log(typeof isAvailable);
+
+//undefined
+ 
+let x;
+console.log(x);
+console.log(typeof x);
+
+//array
+let fruits = ["apple", "banana"];
+console.log(fruits);
+console.log(typeof fruits);
+
+//null
+ let s=null;
+ console.log(s);
+ console.log(typeof s);
+
+
+ //3 Numbers
+ //sum
+ var a=20;
+ var b=13;
+ var c=a+b;
+ console.log(c);
+
+ //difference
+
+ var a=20;
+ var b=13;
+ var c=a-b;
+ console.log(c);
+
+ //product
+
+ var a=20;
+ var b=13;
+ var c=a*b;
+ console.log(c);
+
+ //division
+
+ var a=20;
+ var b=10;
+ var c=a/b;
+ console.log(c);
+
+ //4.Strings
+
+ let k="srini";
+ let v=" vas";
+ let m=k+v;
+ console.log(m);
+
+ let l = "srinivas";
+let i = "konchada";
+let result = l.concat(" ",i);
+console.log(result);
+
+//5thBooleans
+let isJavaScriptFun=true;
+let isCodeHard =false;
+let isLearning=true;
+ let r=isJavaScriptFun && isCodeHard;
+ console.log(r);
+  let o=isCodeHard || isLearning;
+  console.log(o);
+
+  //6thArrays
+  let foods=["biryani","chicken","eggs"];
+   console.log(foods[0]);
+   console.log(foods.length);
+   
+   foods.push("panipuri");
+   console.log(foods);
+
+   //simple math 1
+   var temperature=35;
+   var a=5;
+   var c=2;
+   var mult=3;
+   var div=2;
+   var b=temperature+a;
+   var sub=b-c;
+   var multi=sub*mult;
+   var divide=multi/2;
+   console.log(divide);
+      //simple math2
+      var temperature=35;
+      var add=temperature+5;
+      console.log(add);
+      var substract=temperature-2;
+      console.log(substract);
+      var multiply=temperature*3;
+      console.log(multiply);
+      var divide=temperature/2;
+      console.log(divide);
+      //8.varible names valid or notvalid
+      // _firstName - Valid
+       // $price  -Valid
+
+        //9.Comments
+        //single line
+//we are learning in datatypes and varibles. it's single line comment.
+//multiline
+/* we learning after datatypes concepts 
+ operators and doing one task */
+
+ //10.data type Checking
+ let Str="Hello World";
+ console.log(typeof Str);
+ let numb=42;
+ console.log(typeof numb);
+ let hero=true;
+ console.log(typeof hero);
+ let arr=[1,2,3];
+ console.log(typeof arr);
+ let un;
+ console.log(typeof un);
+
+ //11.basic console
+ console.log("Hello, JavaScript!");
+ console.info("Hello, JavaScript!");
+ console.debug("Hello, JavaScript!");
+ 
+
+ //12.Varibles and Numbers
+ var z=20;
+ var n=30;
+ var add=z+n;
+ console.log(add);
+ var add=z+n;
+ var sub=z-n;
+
+ console.log(sub);
+ var mult=z*n;
+ console.log(mult);
+
+ var divided=z/n;
+ console.log(divided);
+  
+ console.log(z/0);
+ console.log(n/0);
+
+
+
+
+
